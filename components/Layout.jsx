564884e30cbe9ac22e2c3cda086167a05528ceae
Hugo Farji <hugo.farji@wolox.com.ar>
2018-04-14 17:17:54 -0300

    Updated serviceworker

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -13,13 +13,14 @@ class Layout extends Component {
       this.setState({showSidebar: false})
     }
     if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./sw.js').then(registration => {
-          console.log('SW registered: ', registration);
-        }).catch(registrationError => {
-          console.log('SW registration failed: ', registrationError);
-        });
-      });
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then(registration => {
+          console.log('service worker registration successful')
+        })
+        .catch(err => {
+          console.warn('service worker registration failed', err.message)
+        })
     }
   }
 
--- next.config.js
+++ next.config.js
@@ -1,40 +1,32 @@
 const withSass = require('@zeit/next-sass')
-
-const merge = require('webpack-merge');
-const webpack = require('webpack');
-
-const WorkboxPlugin = require('workbox-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path')
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
+// const merge = require('webpack-merge');
+// const webpack = require('webpack');
+//
+// const WorkboxPlugin = require('workbox-webpack-plugin');
+// const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = withSass({
-  webpack: (config) => {
-    config.plugins.push(
-      new WebpackPwaManifest({
-        name: 'Money Converter',
-        short_name: 'money-converter',
-        description: 'A cool money converter!',
-        background_color: '#00c6ff',
-        theme_color: '#00c6ff',
-        icons: [
-          {
-            src: 'https://hdf1986.github.io/money-converter/icon_128x128.ea4e09e0a110e8b409a87b9ec2ae8702.png',
-            sizes: [96, 128, 192, 256, 384, 512]
-          }
-        ]
-      })
-    );
-    config.plugins.push(
-      new WorkboxPlugin({
-        clientsClaim: true,
-        skipWaiting: true
-      })
-    )
-    // config.plugins.push(
-    //   new webpack.optimize.CommonsChunkPlugin({
-    //     name: 'manifest'
-    //   })
-    // )
-
-    return config;
+  webpack: (config, {dev}) => {
+    if(!dev){
+      config.plugins.push(new SWPrecacheWebpackPlugin({
+        cacheId: 'test-lighthouse',
+        filepath: path.resolve('./static/sw.js'),
+        staticFileGlobs: [
+          'static/**/*'
+        ],
+        minify: true,
+        staticFileGlobsIgnorePatterns: [/\.next\//],
+        runtimeCaching: [{
+          handler: 'fastest',
+          urlPattern: /[.](png|jpg|css)/
+        },{
+          handler: 'networkFirst',
+          urlPattern: /^http.*/
+        }]
+      }))
+    }
+    return config
   }
 })
