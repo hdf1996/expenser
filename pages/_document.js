d4f9c73b3c217be10e44c6c75ef0c168532ccb1e
Hugo Farji <hugo.farji@wolox.com.ar>
2018-04-14 18:12:03 -0300

    Added alias

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,16 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="stylesheet" href="/_next/static/style.css" />
+
+            <link rel="manifest" href="/static/manifest.webmanifest" />
+
+            <meta name="theme-color" content="#ff6600" />
+            <link rel="shortcut icon" href="/static/icon.png" />
+            <link rel="apple-touch-icon" href="/static/icon.png" />
+            <meta name="apple-mobile-web-app-title" content="Hacker News" />
+            <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+            <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta name="mobile-web-app-capable" content="yes" />
         </Head>
         <body>
           <Main />
