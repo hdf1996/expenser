8b90f6113c8815a82e082a53ca5194253b76b12d
Hugo Farji <hugo.farji@wolox.com.ar>
2018-04-14 13:18:32 -0300

    Test for pwa

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -12,6 +12,15 @@ class Layout extends Component {
     Router.onRouteChangeStart = url => {
       this.setState({showSidebar: false})
     }
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js').then(registration => {
+          console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+          console.log('SW registration failed: ', registrationError);
+        });
+      });
+    }
   }
 
   render () {
--- next.config.js
+++ next.config.js
@@ -1,2 +1,40 @@
 const withSass = require('@zeit/next-sass')
-module.exports = withSass()
+
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+
+module.exports = withSass({
+  webpack: (config) => {
+    config.plugins.push(
+      new WebpackPwaManifest({
+        name: 'Money Converter',
+        short_name: 'money-converter',
+        description: 'A cool money converter!',
+        background_color: '#00c6ff',
+        theme_color: '#00c6ff',
+        icons: [
+          {
+            src: 'https://hdf1986.github.io/money-converter/icon_128x128.ea4e09e0a110e8b409a87b9ec2ae8702.png',
+            sizes: [96, 128, 192, 256, 384, 512]
+          }
+        ]
+      })
+    );
+    config.plugins.push(
+      new WorkboxPlugin({
+        clientsClaim: true,
+        skipWaiting: true
+      })
+    )
+    // config.plugins.push(
+    //   new webpack.optimize.CommonsChunkPlugin({
+    //     name: 'manifest'
+    //   })
+    // )
+
+    return config;
+  }
+})
